4f38791d9c348598ac89de2c647c27831b48fba0
viniciusdev720 <testenedfor@gmail.com>
2026-08-18 22:09:31 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://oqfyfnsilmmzcbmapewe.supabase.co";
-const SUPABASE_KEY = "sb_publishable_mUJ1JgffI1pgtzZckF4kkw_968BuDfV";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-console.log("Supabase conectado!");
